64a90fee2f3eb3f610189e57bee7260666539e01
Matt Johnson <matthew.d.johnson8@gmail.com>
2019-03-20 04:36:22 +0000

    add auth

--- Gemfile
+++ Gemfile
@@ -72,3 +72,9 @@ end
 
 gem 'will_paginate', '>= 3.1'
 
+gem 'simple_form'
+
+gem "font-awesome-rails"
+
+gem 'devise'
+
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,19 @@ class PlacesController < ApplicationController
   	@places = Place.paginate(page: params[:page], per_page: 2)
   end
 
+  def new
+  	@place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
